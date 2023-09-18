42c8d91eb1597225a1caeec30d707fe956a3cff7
Cosmin_Iulian_Babarici <cosminiulianbabarici192001@gmail.com>
2023-09-18 09:41:26 +0200

    Practica2

---
+++ Practica2/ejercicio2.ps1
@@ -0,0 +1,12 @@
+﻿[int] $num1= Read-Host "Dime el primer número"
+[int] $num2= Read-Host "Dime el segundo número"
+$resultado= $num1+$num2
+Write-Host "La suma de los dos números es" $resultado
+$resultado= $num1-$num2
+Write-Host "La resta de los dos números es" $resultado
+$resultado= $num1*$num2
+Write-Host "La multiplicación de los dos números es" $resultado
+$resultado= $num1/$num2
+Write-Host "La división de los dos números es" $resultado
+$resultado= $num1%$num2
+Write-Host "El resto de los dos números es" $resultado
---
+++ Practica2/ejercicio3.ps1
@@ -0,0 +1,4 @@
+﻿[int]$horas = Read-Host "Cuantas horas has trabajado este mes?"
+[int]$coste = Read-Host "A cuanto cobras la hora?"
+$salario = $horas*$coste
+Write-Host "Tu salario son" $salario "euros" 
