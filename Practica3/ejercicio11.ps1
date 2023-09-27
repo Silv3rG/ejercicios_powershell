7aab77c458ea03a643a0f9b6194442afc69d2fc7
Cosmin Iulian Babarici <126715606+Silv3rG@users.noreply.github.com>
2023-09-27 15:26:19 +0200

    Ejercicio 11 y 12

---
+++ Practica3/ejercicio11.ps1
@@ -0,0 +1,7 @@
+﻿$edad = Read-Host "¿Cuantos años tienes?"
+
+switch($edad){
+{$_ -lt 4} {Write-Host "Puedes entrar gratis"}
+{($_ -ge 4) -and ($_ -le 18)} {Write-Host "La entrada cuesta 5€"}
+{$_ -gt 18}  {Write-Host "La entrada cuesta 10€"}
+}
