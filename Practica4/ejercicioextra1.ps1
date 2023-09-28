10637922e5ae3c4199684b7274cc8d3ecd4c419c
Cosmin_Iulian_Babarici <cosminiulianbabarici192001@gmail.com>
2023-09-28 09:18:25 +0200

    a

--- Practica4/ejercicioextra1.ps1
+++ Practica4/ejercicioextra1.ps1
@@ -1,9 +1,9 @@
 ﻿$num1 = Read-Host "Dime un número: "
 for($a=1; $a -le $num1; $a++){
-$b = (2*$a-1)
+$b = ($a)
 while ($b -ge 1){
 Write-Host "$b " -NoNewline
-$b= $b-2
+$b= $b-1
 }
 Write-Host ""
 
---
+++ Practica4/ejercicioextra2.ps1
@@ -0,0 +1,10 @@
+﻿$num1 = Read-Host "Dime un número: "
+for($a=1; $a -le $num1; $a++){
+$b = ($a)
+while ($b -gt 1){
+Write-Host "-" -NoNewline
+$b= $b-1
+}
+Write-Host "*"
+
+}
