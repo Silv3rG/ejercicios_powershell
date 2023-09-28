0d9f39a282b7e669ec7f9427e1773f18319878cc
Cosmin_Iulian_Babarici <cosminiulianbabarici192001@gmail.com>
2023-09-28 08:26:36 +0200

    a

--- Practica4/ejercicio8.ps1
+++ Practica4/ejercicio8.ps1
@@ -1,11 +1,9 @@
 ﻿$num1 = Read-Host "Dime un número: "
 for($a=1; $a -le $num1; $a++){
-$b = 1
-while ($b -le $a){
-$c= (2*$b-1)
-Write-Host "$c " -NoNewline
-$b++
-
+$b = (2*$a-1)
+while ($b -ge $1){
+Write-Host "$b " -NoNewline
+$b= $b-2
 }
 Write-Host ""
 
---
+++ Practica4/ejercicio8real.ps1
@@ -0,0 +1,7 @@
+﻿[int] $altura = Read-Host "Introduce un número"
+for ($a= 1; $a -le $altura; $a++){
+for($num= 2*$a-1;$num -ge 1; $num = $num -2){
+Write-Host -NoNewline "$num "
+}
+Write-Host""
+}
