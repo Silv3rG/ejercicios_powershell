d28850d2729e0294b3fb5723b7f19ef3564c7840
Cosmin_Iulian_Babarici <cosminiulianbabarici192001@gmail.com>
2023-10-02 08:52:43 +0200

    Practica5

---
+++ Practica5/ejercicio1.ps1
@@ -0,0 +1,43 @@
+﻿Function Sumar($x, $y) {
+    $sumar = $x + $y
+    Write-Host "La respuesta es $sumar"
+    }
+
+Function Restar ($x, $y) {
+    $restar = $x - $y
+    Write-Host "La respuesta es $restar"
+    }
+
+Function Multiplicar ($x, $y) {
+    $multiplicar = $x * $y
+    Write-Host "La respuesta es $multiplicar"
+    }
+
+Function Dividir ($x, $y) {
+    $dividir = $x / $y
+    Write-Host "La respuesta es $dividir"
+    }
+Clear-Host
+while($operacion -ine "Salir") {
+
+$operacion = Read-Host "
+***** Calculador*****
+Sumar
+Restar
+Multiplicar
+Dividir
+Salir
+¿Qué desea hacer?Elige una opción"
+[int]$num1 = Read-Host "Dime un número"
+[int]$num2 = Read-Host "Dime otro número"
+
+switch($operacion){
+
+{$_ -ieq "Sumar"}{ Sumar $num1 $num2}
+{$_ -ieq "Restar"}{ Restar $num1 $num2}
+{$_ -ieq "Multiplicar"}{ Multiplicar $num1 $num2}
+{$_ -ieq "Dividir"}{ Dividir $num1 $num2}
+{$_ -ieq "Salir"}{ exit}
+}
+
+}
---
+++ Practica5/ejercicio2.ps1
@@ -0,0 +1,6 @@
+﻿$usuarios = Import-Csv Z:\Practica5\usuarios.csv
+
+foreach ($usu in $usuarios)
+{
+Write-Host " Usuario: $($usu.nombre) $($usu.apellidos)   $($usu.grupo)"
+}
